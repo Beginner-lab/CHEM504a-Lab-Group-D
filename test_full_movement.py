a7c6250523d336e8acd473ebc1baa9fc9f27fa6e
Alexander Chan <alexanderbhchan@gmail.com>
2025-03-05 11:04:26 +0000

    Update test_full_movement.py

--- test_full_movement.py
+++ test_full_movement.py
@@ -70,25 +70,24 @@ def main_5(robot, gripper):
     joint_state = [1.3438105583190918, -1.5019107994488259, 1.933467213307516, -2.010717054406637, -1.5371387640582483, 0.061162471771240234]
     robot.move_joint_list(joint_state, 0.25, 0.5, 0.02)
     gripper.move(0,125,125)
-
-def main_6(robot, gripper):
     joint_state = [1.3438812494277954, -1.5379605305245896, 1.8642199675189417, -1.905468603173727, -1.536783520375387, 0.0609099380671978]
     robot.move_joint_list(joint_state, 0.25, 0.5, 0.02)
+    time.sleep(10)
 
-def main_7(robot, gripper):
+def main_6(robot, gripper):
     joint_state = [1.3438105583190918, -1.5019107994488259, 1.933467213307516, -2.010717054406637, -1.5371387640582483, 0.061162471771240234]
     robot.move_joint_list(joint_state, 0.25, 0.5, 0.02)
     gripper.move(255,125,125)
 
-def main_8(robot, gripper):
+
     joint_state = [1.3439116477966309, -1.585302015344137, 1.6429570356952112, -1.636733194390768, -1.5360587278949183, 0.0600738525390625]
     robot.move_joint_list(joint_state, 0.25, 0.5, 0.02)
 
-def main_9(robot, gripper):
+
     joint_state = [1.1097872257232666, -1.1103881162456055, 1.6571067015277308, -2.1457120380797328, -1.5382736364947718, -0.5338419119464319]
     robot.move_joint_list(joint_state, 0.25, 0.5, 0.02)
 
-def main_10(robot, gripper):
+
     joint_state = [1.1097524166107178, -1.0942512613585968, 1.6702635923968714, -2.174934049645895, -1.538393799458639, -0.5338013807879847]
     robot.move_joint_list(joint_state, 0.25, 0.5, 0.02)
     gripper.move(0,125,125)
@@ -98,9 +97,5 @@ if __name__=="__main__":
      main_2(robot_1, gripper)
      main_3(robot_1, gripper)
      main_4(robot_1, gripper)
-     main_5(robot_1, gripper)
-     main_6(robot_1, gripper)
-     main_7(robot_1, gripper)
-     main_8(robot_1, gripper)
-     main_9(robot_1, gripper)
-     main_10(robot_1, gripper)
+     main_5(robot_1 ,gripper)
+     main_6(robot_1 ,gripper)
